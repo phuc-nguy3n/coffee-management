1a6b8e1620203f057816ec0c2f3f6c866392c896
phucNguyen <nguyenphucvt112@gmail.com>
2026-02-06 19:09:40 +0700

    Fix

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -12,13 +12,12 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth
 // Cấu hình Firebase của riêng bạn (Lấy từ Firebase Console)
 // Lưu ý: Hãy thay thế các giá trị bên dưới bằng mã bạn copy được từ Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyDdjiuYsxXA-TPYEajWcKECoaG3g569B-c",
-  authDomain: "coffee-management-ff2a2.firebaseapp.com",
-  projectId: "coffee-management-ff2a2",
-  storageBucket: "coffee-management-ff2a2.firebasestorage.app",
-  messagingSenderId: "255370865286",
-  appId: "1:255370865286:web:93538bb6f6f50eae5b3f07",
-  measurementId: "G-4TTPLYX0EL",
+  apiKey: "YOUR_API_KEY_HERE",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 // Khởi tạo ứng ụng
